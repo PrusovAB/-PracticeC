1a20ea443025ed14802bd3e424c10c89828f68f5
PrusovAB <djromeo93@mail.ru>
2022-07-09 14:11:07 +0300

    add Exercise03

--- Exercise03/Program.cs
+++ Exercise03/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne == 0)
+{
+	Console.WriteLine($"Самое четное число их всех чисел = {numberOne}");
+}
+else
+{
+	if (numberOne % 2 == 0)
+	{
+		Console.WriteLine($"Введеное число = {numberOne} четное");
+	}
+	else
+	{
+		Console.WriteLine($"Введеное число = {numberOne} не четное");
+	}
+}
