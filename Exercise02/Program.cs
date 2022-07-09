1410b0b12e6fb4e642db2c698a579af121ef8fdf
PrusovAB <djromeo93@mail.ru>
2022-07-09 14:00:41 +0300

    Exercise02

--- Exercise02/Program.cs
+++ Exercise02/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+int numberThree = Convert.ToInt32(Console.ReadLine());
+
+
+int max = numberOne;
+
+
+if (numberTwo > max)
+{
+	max = numberTwo;
+}
+if (numberThree > max)
+{
+	max = numberThree;
+}
+Console.WriteLine($"Максимальное число среди этих чисел = {max}");
