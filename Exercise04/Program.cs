54d246249b21ff03ba7bb3f0c7d5e125206a14f7
PrusovAB <djromeo93@mail.ru>
2022-07-09 14:40:48 +0300

    add Exercise04

--- Exercise04/Program.cs
+++ Exercise04/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne <= 1)
+{
+	Console.WriteLine("Число нужно целое больше 1: ");
+}
+else
+{
+	for (int i = 1; i <= numberOne; i++)
+	{
+		if (i % 2 == 0)
+		{
+			Console.Write($" {i}");
+
+		}
+	}
+}
+
+
