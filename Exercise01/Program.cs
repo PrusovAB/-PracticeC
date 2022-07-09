266fa4ce115e6bbfeb0422d16685e04aeca112e0
PrusovAB <djromeo93@mail.ru>
2022-07-09 13:32:44 +0300

    add ExerciceC

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -1,2 +1,19 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numberOne > numberTwo)
+{
+	Console.WriteLine($"Максимальное число {numberOne}");
+}
+else if (numberOne < numberTwo)
+{
+	Console.WriteLine($"Максимальное число {numberTwo}");
+}
+else
+{
+	Console.WriteLine($"Числа  {numberOne} и {numberTwo} ровны");
+}
+
